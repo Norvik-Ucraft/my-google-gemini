438e05e8b8f7d5b560f34f3c0fde9c6e54f3e517
Norvik Alexian <norvik@ucraft.com>
2024-04-11 15:28:35 +0400

    loaded new environment variables

--- config.py
+++ config.py
@@ -5,3 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+VERTEXAI_JSON_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+PROJECT_ID = os.getenv("PROJECT_ID")
+LOCATION = os.getenv("LOCATION")
