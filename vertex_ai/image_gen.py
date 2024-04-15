167d5a003bdaca18b84981186fb77e172a4c93ec
Norvik Alexian <norvik@ucraft.com>
2024-04-15 17:01:49 +0400

    added logging for better error handling

--- vertex_ai/image_gen.py
+++ vertex_ai/image_gen.py
@@ -1,4 +1,6 @@
 import uuid
+import logging
+from typing import Optional
 
 import vertexai
 from vertexai.preview.vision_models import ImageGenerationModel
@@ -10,20 +12,24 @@ vertexai.init(project=config.PROJECT_ID, location=config.LOCATION)
 generation_model = ImageGenerationModel.from_pretrained("imagegeneration@006")
 
 
-def generate_icon(keywords_list: list) -> str:
+def generate_icon(keywords_list: list) -> Optional[str]:
     """
     Generate an icon from provided prompt.
     :param keywords_list: Keywords that we got from users.
     :return: generated image unique id
     """
-    keywords = ", ".join(keywords_list)
-    prompt = constants.IMAGEN_MODEL_PROMPT.replace("[keywords]", keywords)
-    icon_id = uuid.uuid4()
+    try:
+        keywords = ", ".join(keywords_list)
+        prompt = constants.IMAGEN_MODEL_PROMPT.replace("[keywords]", keywords)
+        icon_id = uuid.uuid4()
 
-    response = generation_model.generate_images(
-        prompt=prompt,
-        safety_filter_level="block_most",
-    )
+        response = generation_model.generate_images(
+            prompt=prompt,
+            safety_filter_level="block_most",
+        )
 
-    response.images[0].save(f"./outputs/photos/{icon_id}.png")
-    return icon_id
+        response.images[0].save(f"./outputs/photos/{icon_id}.png")
+        return icon_id
+    except Exception as e:
+        message = f"Something went wrong with generating icons, message: {e}"
+        logging.error(message, exc_info=True)
