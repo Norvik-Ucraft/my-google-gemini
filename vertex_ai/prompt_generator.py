64371da06efb85c6126067e7f645bcfc9dd6b079
Norvik Alexian <norvik@ucraft.com>
2024-04-15 14:53:34 +0400

    updates in the prompt generator module

--- vertex_ai/prompt_generator.py
+++ vertex_ai/prompt_generator.py
@@ -2,29 +2,24 @@ import os
 
 import vertexai
 from dotenv import load_dotenv
-from vertexai.preview.generative_models import GenerativeModel
+from vertexai.preview.generative_models import GenerativeModel, GenerationConfig
 
 import config
+from vertex_ai import constants
 
 load_dotenv()
 
 os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
 
-prompt = """
-Generate one prompt for imagen model to represents pure icon or a symbol of coffee which will be used later in a logo. \
-** Important rules to follow: \
-1. The icon should always be with white background. \
-2. The icon should always be without any text and without any shadows. \
-Return the prompt without any extra text or description.
-"""
 
-
-def generate_text(model_prompt: str) -> str:
+def generate_prompt(keywords: list) -> str:
     vertexai.init(project=config.PROJECT_ID, location=config.LOCATION)
     multimodal_model = GenerativeModel("gemini-1.5-pro-preview-0409")
 
     # ToDo Configure the model if it's necessary
-    # generation_config = GenerationConfig(temperature=1.)
+    generation_config = GenerationConfig(max_output_tokens=32, stop_sequences="\n")
 
-    response = multimodal_model.generate_content(contents=[model_prompt])
+    response = multimodal_model.generate_content(
+        contents=[f"{constants.PROMPT_GENERATOR_PROMPT}\nKeywords: {keywords}"], generation_config=generation_config
+    )
     return response.text
