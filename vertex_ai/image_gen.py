decc0b561f991e9431ed829b08666f2f93b17c7e
Norvik Alexian <norvik@ucraft.com>
2024-04-17 16:01:36 +0400

    define new function to make the background transparent

--- vertex_ai/image_gen.py
+++ vertex_ai/image_gen.py
@@ -3,6 +3,7 @@ import logging
 from typing import Optional
 
 import vertexai
+from PIL import Image
 from vertexai.preview.vision_models import ImageGenerationModel
 
 import config
@@ -26,9 +27,25 @@ def generate_icon(keywords_list: list) -> Optional[str]:
         response = generation_model.generate_images(
             prompt=prompt, safety_filter_level="block_most", negative_prompt="low quality"
         )
+        image_path = f"./outputs/transparent_bg/{icon_id}.png"
+        response.images[0].save(image_path)
 
-        response.images[0].save(f"./outputs/photos/{icon_id}.png")
-        return icon_id
+        return image_path
     except Exception as e:
         message = f"Something went wrong with generating icons, message: {e}"
         logging.error(message, exc_info=True)
+
+
+def make_white_background_transparent(image_path: str, tolerance: int = 20) -> None:
+    image = Image.open(image_path)
+    image = image.convert("RGBA")
+    bg_color = image.getpixel((0, 0))
+
+    width, height = image.size
+    for x in range(width):
+        for y in range(height):
+            pixel_color = image.getpixel((x, y))
+            if all(abs(pixel_color[i] - bg_color[i]) <= tolerance for i in range(3)):
+                image.putpixel((x, y), (0, 0, 0, 0))
+
+    image.save("transparent_image.png", format="PNG")
