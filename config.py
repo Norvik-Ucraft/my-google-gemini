1f50b8d1c52f6529426e92d830aa4e7a024915f3
Norvik Alexian <norvik@ucraft.com>
2024-10-07 20:29:06 +0400

    update the config module

--- config.py
+++ config.py
@@ -1,8 +1,16 @@
 import os
+import logging
 
 from dotenv import load_dotenv
 
 load_dotenv()
+
+LOGGING_LEVEL = os.getenv("LOGGING_LEVEL", "INFO")
+logging.basicConfig(
+    level=logging.getLevelNamesMapping()[LOGGING_LEVEL],
+    format="%(asctime)s:%(name)s:%(levelname)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 VERTEXAI_JSON_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
