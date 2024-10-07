fe29612df24126694b6ef3290466acfd9d683a68
Norvik Alexian <norvik@ucraft.com>
2024-10-07 20:26:39 +0400

    updates in the run module

--- run.py
+++ run.py
@@ -1,8 +1,18 @@
 from vertex_ai import image_gen
 
-prompt_keywords = ["cheese burger", "fastfood"]
+prompt_keywords = ["tech", "betting", "money"]
 
-generated_icon = image_gen.generate_icon(keywords_list=prompt_keywords)
+generated_icon = image_gen.generate_icon(keywords_list=prompt_keywords, name="Alexian")
 image_gen.make_white_background_transparent(image_path=generated_icon)
-
 print(generated_icon)
+
+tested = """
+["angus", "steak", "restaurant", "cozy"],
+["payment gateway", "online transaction"],
+["chinese food", "noodle", "soup"],
+["wine", "glass", "bottle", "waiter"],
+["italian", "pizza"],
+["football", "mascot"],
+["football", "mascot", "eagle"],
+["Spanish", "bachata", "concert hall"]
+"""
